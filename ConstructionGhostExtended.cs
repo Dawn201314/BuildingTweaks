eeb9129d3fde7957f84605491cfc917642c88a76
O-S Marin <K07H@users.noreply.github.com>
2022-10-24 19:02:19 +0200

    Fixed bug
    It is now possible to use Instant-Build feature and Finish-Blueprints shortcut at the same time.

--- ConstructionGhostExtended.cs
+++ ConstructionGhostExtended.cs
@@ -6,7 +6,7 @@ namespace InstantBuild
     {
         protected override void Update()
         {
-            if (InstantBuild.FinishBlueprintsEnabled && Input.GetKeyDown(InstantBuild.ModKeybindingId_Finish) && ((P2PSession.Instance.GetGameVisibility() == P2PGameVisibility.Singleplayer || ReplTools.AmIMaster()) || (InstantBuild.PermissionGranted && !InstantBuild.PermissionDenied)) && this.GetState() == GhostState.Building)
+            if (InstantBuild.FinishBlueprintsEnabled && Input.GetKeyDown(InstantBuild.ModKeybindingId_Finish) && this.GetState() == GhostState.Building && ((P2PSession.Instance.GetGameVisibility() == P2PGameVisibility.Singleplayer || ReplTools.AmIMaster()) || (InstantBuild.PermissionGranted && !InstantBuild.PermissionDenied)))
             {
                 Player p = Player.Get();
                 if (p != null && InstantBuild.IsWithinRadius(p.GetWorldPosition(), this.transform.position))
@@ -15,8 +15,17 @@ namespace InstantBuild
                     if (!this.ReplIsOwner())
                         this.ReplRequestOwnership(false);
                     this.ReplSetDirty();
+                    this.ReplSendAsap();
                     Cheats.m_InstantBuild = true;
                     this.SetState(GhostState.Building);
+                    this.ReplSetDirty();
+                    this.ReplSendAsap();
+                    if (this.GetState() != GhostState.Ready)
+                    {
+                        this.SetState(GhostState.Building);
+                        this.ReplSetDirty();
+                        this.ReplSendAsap();
+                    }
                     Cheats.m_InstantBuild = instantBuildBkp;
 #if VERBOSE
                     ModAPI.Log.Write($"[{InstantBuild.ModName}:ConstructionGhostExtended.Update] Blueprint \"{this.GetName()}\" was built instantly at [{this.transform.position.x.ToString("F1")};{this.transform.position.y.ToString("F1")};{this.transform.position.z.ToString("F1")}].");
